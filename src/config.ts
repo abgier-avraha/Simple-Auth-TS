3a04ec7dd3e8163ace77d2af7e04e168f7130400
Abgier Avraha <abgier.avraha@gmail.com>
2025-10-10 03:54:05 +1100

    encryption test

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,11 @@ export interface SimpleAuthPublicClientConfig<TSession, TState> {
 	client_id: string;
 	redirect_uri: string;
 	scope: string[];
+	// Serialises the session before storing
 	sessionSerialiser: ISerializer<TSession>;
+	// Serialises the state before storing
 	stateSerialiser: ISerializer<TState>;
+	// For storing the serialised state or session
 	storage: IStorage;
 }
 
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,25 @@
-import { expect, test } from "vitest";
+import { expect, test, vi } from "vitest";
+import { getDefaultEncryptedSerializer } from "./serialization.js";
 
-test("adds 1 + 2 to equal 3", () => {
-	expect(1 + 2).toBe(3);
+test("Encryption serializer works", () => {
+	// Mock random bytes to fixed value
+	vi.mock("crypto", async () => {
+		const actual = await vi.importActual("crypto");
+		return {
+			...actual,
+			randomBytes: (size: number) => Buffer.from("a".repeat(size)),
+		};
+	});
+
+	const encryptedSerializer = getDefaultEncryptedSerializer("<key>");
+
+	// Test serialization
+	const serialized = encryptedSerializer.stringify("This is a test.");
+	expect(serialized).toBe(
+		"YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFxNbjMUzaeFFr1a3QN58ZHGQTTpLozdCsg4g10FUEp35yHI27548Xg",
+	);
+
+	// Test parsing
+	const parsed = encryptedSerializer.parse(serialized);
+	expect(parsed).toBe("This is a test.");
 });
